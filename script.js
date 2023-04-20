bd574a8602616dfc9e1368c180687ba697a7a2b1
gusttavares97 <gustavotavares018@gmail.com>
2023-04-20 16:27:24 -0300

    finalizando-js

--- script.js
+++ script.js
@@ -1,59 +1,47 @@
-const btncripto = document.querySelector('button.btn#crip');
-const textarea  = document.querySelector('textarea.texto');
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
 
-
-
-function btn (){
-    const textoEncriptado = criptografar(textarea.value);
+function btnCripto() {
+    const textoEncriptado = encriptar(inputTexto.value);
     mensagem.value = textoEncriptado;
-}
-
-
-function criptografar(stringEncriptada){
-    let codificar = [['e', 'entre'],['i','imes'], ['a' ,'ai'],['o', 'ober'], ['u', 'ufat']];
-    stringEncriptada = stringEncriptada.toLowerCase()
-    stringEncriptada = stringEncriptada.normalize('NFD').replace(/[a-zA-Z]/g, '');
-
-
-   for( let i =0; i< codificar.length; i++){
-    if(stringEncriptada.includes(codificar[i][0])){
-        stringEncriptada = stringEncriptada.replaceAll(codificar[i][0], codificar[i][0])
-    }
-   } 
-return stringEncriptada;
-
-}
-
-function btnDescript(){
-    const textoDescriptado = descriptar(textarea.value);
-    mansagem.value = textoDescriptado;
     mensagem.style.backgroundImage = "none";
 }
 
-function descriptar (stringDescriptada){
-    let descodificar =[ ['ai', 'a'], ['entre', 'e'], ['imes', 'i'], ['ober', 'o'], ['ufat', 'u']];
+function encriptar(stringEncriptada) {
+    let codificar = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    stringEncriptada = stringEncriptada.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
+
+    for (let i = 0; i < codificar.length; i++) {
+        if (stringEncriptada.includes(codificar[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(codificar[i][0], codificar[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDescripto() {
+    const textoDescriptado = descriptar(inputTexto.value);
+    mensagem.value = textoDescriptado;
+    mensagem.style.backgroundImage = "none";
+}
+
+function descriptar(stringDescriptada) {
+    let descodificar = [["ai", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]];
     
-    for(let i = 0; i< descodificar.length; i++){
-        if(stringDescriptada.includes(descodificar[i][0])){
-            stringDescriptada =stringDescriptada.replaceAll(descodificar[i][0], descodificar[i][1]);
+    for (let i = 0; i < descodificar.length; i++) {
+        if(stringDescriptada.includes(descodificar[i][0])) {
+            stringDescriptada = stringDescriptada.replaceAll(descodificar[i][0], descodificar[i][1]);
         }
     }
     return stringDescriptada;
 }
 
-
-function btnCopia(){
-    const textoCopiado = document.querySelector(textarea.mensagem)
+function btnCopiar() {
+    const textoCopiado = document.querySelector(".mensagem");
     textoCopiado.select();
     navigator.clipboard.writeText(textoCopiado.value);
-    window.location.roloand()
-}
-
-
-
-
-btnCripto.onclick = btn;
-document.querySelector('button.btn#descrip').onclick = btnDescript;
-document.querySelector('button.btn-cop').onclick = btnCopia;
+    window.location.reload();
+} 
 
 
