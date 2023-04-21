87a255a8b3f6f18e62994c5af90fa19c22bcddd7
gusttavares97 <gustavotavares018@gmail.com>
2023-04-21 10:33:28 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const inputTexto = document.querySelector(".input-texto");
 const mensagem = document.querySelector(".mensagem");
 
+
 function btnCripto() {
     const textoEncriptado = encriptar(inputTexto.value);
     mensagem.value = textoEncriptado;
     mensagem.style.backgroundImage = "none";
 }
 
+
 function encriptar(stringEncriptada) {
     let codificar = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
     stringEncriptada = stringEncriptada.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
+
 
     for (let i = 0; i < codificar.length; i++) {
         if (stringEncriptada.includes(codificar[i][0])) {
@@ -26,6 +29,7 @@ function btnDescripto() {
     mensagem.style.backgroundImage = "none";
 }
 
+
 function descriptar(stringDescriptada) {
     let descodificar = [["ai", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]];
     
@@ -36,6 +40,7 @@ function descriptar(stringDescriptada) {
     }
     return stringDescriptada;
 }
+
 
 function btnCopiar() {
     const textoCopiado = document.querySelector(".mensagem");
